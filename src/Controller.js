9d193bdc96601889db31bfad580318fc1b54d94e
marudor <marudor@marudor.de>
2017-05-05 18:26:05 +0200

    wagenreihung

--- src/Controller.js
+++ src/Controller.js
@@ -4,21 +4,21 @@ import axios from 'axios';
 import Memcache from 'memcache-plus';
 
 const memcache = new Memcache({
-  hosts: [process.env.MEMCACHED_SERVER],
+  hosts: [process.env.MEMCACHED_SERVER]
 });
 
 const router = new KoaRouter();
 
 function encodeSearchTerm(term: string) {
   return term
-  .replace(/ü/g, 'ue')
-  .replace(/Ü/g, 'UE')
-  .replace(/ä/g, 'ae')
-  .replace(/Ä/g, 'AE')
-  .replace(/ö/g, 'oe')
-  .replace(/Ö/g, 'OE')
-  .replace(/ß/g, 'ss')
-  .replace(/\$SLASH\$/g, '/');
+    .replace(/ü/g, 'ue')
+    .replace(/Ü/g, 'UE')
+    .replace(/ä/g, 'ae')
+    .replace(/Ä/g, 'AE')
+    .replace(/ö/g, 'oe')
+    .replace(/Ö/g, 'OE')
+    .replace(/ß/g, 'ss')
+    .replace(/\$SLASH\$/g, '/');
 }
 
 async function stationInfo(station: number) {
@@ -26,38 +26,60 @@ async function stationInfo(station: number) {
   if (cached) {
     return cached;
   }
-  const info = (await axios.get(`https://si.favendo.de/station-info/rest/api/station/${station}`)).data;
+  const info = (await axios.get(
+    `https://si.favendo.de/station-info/rest/api/station/${station}`
+  )).data;
   return {
     id: info.id,
     title: info.title,
     evaId: info.eva_ids[0],
-    recursive: info.eva_ids.length > 1,
+    recursive: info.eva_ids.length > 1
   };
 }
 
+// https://ws.favendo.de/wagon-order/rest/v1/si/1401
+async function wagenReihung(trainNumbers: string[], station: number) {
+  const info: Wagenreihung = (await axios.post(
+    `https://ws.favendo.de/wagon-order/rest/v1/si/${station}`,
+    trainNumbers.map(trainNumber => ({ trainNumber }))
+  )).data;
+  return info;
+}
+
 router
-.prefix('/api')
-// https://si.favendo.de/station-info/rest/api/search?searchTerm=Bochum
-.get('/search/:searchTerm', async ctx => {
-  const { searchTerm } = ctx.params;
-  const stations = (await axios.get(`https://si.favendo.de/station-info/rest/api/search?searchTerm=${encodeSearchTerm(searchTerm)}`)).data;
-  ctx.body = stations.map(station => ({
-    title: station.title,
-    id: station.id,
-  }));
-})
-// https://si.favendo.de/station-info/rest/api/station/724
-.get('/station/:station', async ctx => {
-  const { station } = ctx.params;
-  ctx.body = await stationInfo(station);
-})
-.get('/abfahrten/:station', async ctx => {
-  const { station } = ctx.params;
-  const info = await stationInfo(station);
-  // https://marudor.de/api/KD?mode=marudor&backend=iris&version=2
-  const abfahrten = (await axios.get(`http://***REMOVED***f.finalrewind.org/${info.evaId}?mode=marudor&backend=iris&version=2`)).data;
-  ctx.body = abfahrten;
-})
-;
+  .prefix('/api')
+  // https://si.favendo.de/station-info/rest/api/search?searchTerm=Bochum
+  .get('/search/:searchTerm', async ctx => {
+    const { searchTerm } = ctx.params;
+    const stations = (await axios.get(
+      `https://si.favendo.de/station-info/rest/api/search?searchTerm=${encodeSearchTerm(searchTerm)}`
+    )).data;
+    ctx.body = stations.map(station => ({
+      title: station.title,
+      id: station.id
+    }));
+  })
+  // https://si.favendo.de/station-info/rest/api/station/724
+  .get('/station/:station', async ctx => {
+    const { station } = ctx.params;
+    ctx.body = await stationInfo(station);
+  })
+  .get('/abfahrten/:station', async ctx => {
+    const { station } = ctx.params;
+    const info = await stationInfo(station);
+    // https://marudor.de/api/KD?mode=marudor&backend=iris&version=2
+    const abfahrten = (await axios.get(
+      `http://***REMOVED***f.finalrewind.org/${info.evaId}?mode=marudor&backend=iris&version=2`
+    )).data;
+    ctx.body = abfahrten;
+  })
+  .get('/wagen/:station/:train', async ctx => {
+    const { station, train } = ctx.params;
+    try {
+      ctx.body = await wagenReihung([train], station);
+    } catch (e) {
+      ctx.body = e.response.data;
+    }
+  });
 
 global.koa.use(router.routes());
